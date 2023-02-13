b85872f1b7889d7a9e03d30e52e53cf453b3a9fb
Josh Pollard <124677969+josh-thales@users.noreply.github.com>
2023-02-13 18:10:25 +0000

    Added link to coref website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,10 +22,31 @@
 			>
 				<h1 class="text-white text-center">Explore User Guides</h1>
 				<p class="text-white text-center mt-2 mb-10 opacity-50">
-					Need help? Stuck on how to figure something out? Find out how to use the COREF lab space with these helpful guides
+					Need help? Stuck on how to figure something out? Find out how to use the COREF lab space
+					with these helpful guides
 				</p>
-				<img class="fill-accent opacity-70 w-2/3 px-[25%]" src="/images/guide.svg" alt="Guide document" />
+				<img
+					class="fill-accent opacity-70 w-2/3 px-[25%]"
+					src="/images/guide.svg"
+					alt="Guide document"
+				/>
 			</section>
 		</a>
 	</div>
+	<button
+		class="fixed bottom-0 right-0 flex flex-row items-center m-4 pt-1 px-8 bg-[#2f3541] border border-slate-300 text-white font-semibold rounded-lg"
+		on:click={() => window.open('https://coref-uk.com', 'COREF Site', 'width=1000,height=800')}
+	>
+		<img class="w-20 " src="/images/simple-logo.svg" alt="COREF logo" />
+		<svg
+			class="h-4 translate-y-[-2px] ml-3"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 512 512"
+			fill="#999da4"
+			><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+				d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
+			/></svg
+		>
+		<!-- <p class="text-xs opacity-50 ml-3 leading-2 -translate-y-[2px]">SITE</p> -->
+	</button>
 </main>
--- static/guides/displays.md
+++ static/guides/displays.md
@@ -1,3 +1,17 @@
-# How to use the displays in the lab
+# How to Use the Displays in the Lab
 
-All the displays within the lab are interactive screens
+All the displays within the lab are interactive screens, and will allow you to project your laptop to share content
+
+## Plugging in your Laptop
+
+
+
+## Changing Inputs
+
+
+
+## Resetting Screens Once Finished
+
+Please once you have finished with the displays, we ask that you reset them to the video screensaver.
+
+1. 
