040f1561d98175ca504fbfb5556025cf81ffec5d
Josh Pollard <124677969+josh-thales@users.noreply.github.com>
2023-02-23 14:47:40 +0000

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() =>
+		setTimeout(() => {
+			goto('/');
+		}, 2000)
+	);
+</script>
+
+<h1 class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+	{$page.status}: {$page?.error?.message}
+</h1>
