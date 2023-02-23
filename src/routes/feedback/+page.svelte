678249aca4f3c04828725b2c6dee819469acd1aa
Josh Pollard <124677969+josh-thales@users.noreply.github.com>
2023-02-23 13:49:26 +0000

    Added results button + fallback page

--- src/routes/feedback/+page.svelte
+++ src/routes/feedback/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">	
+<script lang="ts">
 	import { quintOut } from 'svelte/easing';
 	import { crossfade } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
@@ -26,14 +26,27 @@
 </script>
 
 <main class="min-h-screen flex flex-col">
-	<section class="bg-accent p-6 md:p-16 shadow-2xl">
-		<h1 class="text-white text-3xl md:text-6xl">Tell us your experience of the lab</h1>
-		<p class="text-white opacity-50 mt-4 max-w-2xl">
-			Thanks for taking the time to provide feedback for the COREF lab. Your input is so important
-			and appreciated because it helps us make the lab even better. Your thoughts and opinions help
-			us see what we're doing right and where we can improve, so we can make sure the lab is meeting
-			your needs and expectations.
-		</p>
+	<section class="flex flex-row justify-between items-center bg-accent p-6 md:p-16 shadow-2xl">
+		<div>
+			<h1 class="text-white text-3xl md:text-6xl">Tell us your experience of the lab</h1>
+			<p class="text-white opacity-50 mt-4 max-w-2xl">
+				Thanks for taking the time to provide feedback for the COREF lab. Your input is so important
+				and appreciated because it helps us make the lab even better. Your thoughts and opinions
+				help us see what we're doing right and where we can improve, so we can make sure the lab is
+				meeting your needs and expectations.
+			</p>
+		</div>
+		<button
+			class="flex flex-row items-center py-2 px-6 opacity-50 bg-[#454F60] border border-slate-300 text-white font-semibold rounded-lg"
+			on:click={() => window.open('https://app.herotofu.com/forms/2aedf4a0-a7cf-11ed-a31e-753411848f80/submissions', 'Feedback Results', 'width=1000,height=800')}
+		>
+			<svg class="h-4 translate-y-[-0.1rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px" fill="#fff">
+				<path
+					d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9 8 4 8.9 4 10 L 4 20 C 4 21.1 4.9 22 6 22 L 18 22 C 19.1 22 20 21.1 20 20 L 20 10 C 20 8.9 19.1 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 12 13 C 13.1 13 14 13.9 14 15 C 14 16.1 13.1 17 12 17 C 10.9 17 10 16.1 10 15 C 10 13.9 10.9 13 12 13 z"
+				/></svg
+			>
+			<p>Results</p>
+		</button>
 	</section>
 	{#if !submitted}
 		<iframe title="dummyframe" name="dummyframe" id="dummyframe" class="hidden" />
@@ -86,15 +99,11 @@
 					<div class="flex flex-col flex-grow">
 						<p class="font-medium text-lg md:text-2xl mb-4">Where are you?</p>
 						<div class="">
-							<input type="radio" name="where" id="crawley" value="Crawley" required>
-							<label class="text-lg" for="crawley">
-								Crawley
-							</label>
-							<br>
-							<input type="radio" name="where" id="belfast" value="Belfast" required>
-							<label class="text-lg" for="belfast">
-								Belfast
-							</label>
+							<input type="radio" name="where" id="crawley" value="Crawley" required />
+							<label class="text-lg" for="crawley"> Crawley </label>
+							<br />
+							<input type="radio" name="where" id="belfast" value="Belfast" required />
+							<label class="text-lg" for="belfast"> Belfast </label>
 						</div>
 						<label class="font-medium text-lg md:text-2xl mb-4 mt-8" for="negatives">
 							What could we have improved?
--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,7 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
+      fallback: 'index.html'
     }),
   },
   preprocess: vitePreprocess()
